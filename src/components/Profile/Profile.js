fb0d6d1f237320ef7dec0ca0f79e947212913bf9
Andrew-Marty00shenko <andriei.martyshienko@mail.ru>
2020-04-24 02:06:32 +0300

    Add animation hover in ProfilePage

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -8,15 +8,15 @@ const Profile = () => {
         setShowBlock(!showBlock)
     }
     const handleInviseBlock = () => {
-        setShowBlock(!showBlock)
+        setShowBlock(!showBlock);
     }
 
     return (
         <div className="profile-page">
             <div className="profile-avatar">
-                <div className="avatar__setting" onMouseEnter={handleShowBlock} onMouseLeave={handleInviseBlock}>
+                <div onMouseEnter={handleShowBlock} onMouseLeave={handleInviseBlock} className="avatar__setting" >
                     <img src="https://sun9-4.userapi.com/impf/c849336/v849336730/6999e/RZAbcj5kt00.jpg?size=200x0&quality=90&sign=60345877c965fb6f810725b815e3233d" alt="" />
-                    <div className={` ${showBlock ? "settings" : "none"}`}>
+                    <div className={`${showBlock ? 'settings' : 'none'}`}>
                         <li> Обновить фотографию</li>
                         <li> Изменить миниатюру</li>
                         <li> Добавить эффекты</li>
